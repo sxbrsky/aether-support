0697b4bcb206b81646e38d02e09c3cb0b2b62713
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-07-23 15:15:06 +0200

    feat: add generic collection

---
+++ tests/stubs/DummyCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the nulxrd/splx.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Splx\Stubs;
+
+use Splx\AbstractCollection;
+
+class DummyCollection extends AbstractCollection
+{
+
+}

215559bea32eb904a026bd45a04783bf32102844
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-07-23 23:22:57 +0200

    feat: implement map data structure based on hashmap implementation

---
+++ src/MapInterface.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the nulxrd/splx.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Splx;
+
+/**
+ * @template TKey
+ * @template TValue
+ *
+ * @extends \Splx\Collection<TKey, TValue>
+ */
+interface MapInterface extends Collection
+{
+    /**
+     * Associates the specified value with the specified key in this map.
+     *
+     * @param TKey $key
+     *  The key to associate the value with.
+     * @param TValue $value
+     *  The value to be associated with the key.
+     *
+     * @return void
+     */
+    public function put(mixed $key, mixed $value): void;
+
+    /**
+     * Returns the value for a given key.
+     *
+     * @param TKey $key
+     *  The key whose associated value is to be returned.
+     *
+     * @return TValue
+     *  The value to which the specified key is mapped.
+     */
+    public function get(mixed $key): mixed;
+
+    /**
+     * Checks whether the map contains a given key.
+     *
+     * @param TKey $key
+     *  The key to look for.
+     *
+     * @return bool
+     *  <b>True</b> if key found, <b>false</b> otherwise.
+     */
+    public function containsKey(mixed $key): bool;
+
+    /**
+     * Determines whether the map contains a given value.
+     *
+     * @param TValue $value
+     *  The value to look for
+     *
+     * @return bool
+     *  <b>True</b> if value found, <b>false</b> otherwise.
+     */
+    public function containsValue(mixed $value): bool;
+}
---
+++ tests/unit/MapTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the nulxrd/splx.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Splx\Tests\Unit;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Splx\Map;
+use Splx\MapInterface;
+
+#[CoversClass(Map::class)]
+class MapTest extends TestCase
+{
+    protected MapInterface $map;
+
+    protected function setUp(): void
+    {
+        $this->map = new Map();
+    }
+
+    public function testGet(): void {
+        $this->map['john'] = 20;
+        $this->map['jane'] = 30;
+
+        self::assertEquals(20, $this->map->get('john'));
+    }
+
+    public function testPut(): void {
+        $this->map->put('jane', 20);
+        $this->map->put('john', 30);
+
+        self::assertCount(2, $this->map);
+        self::assertEquals(20, $this->map->get('jane'));
+    }
+
+    public function testContainsKey(): void {
+        $this->map->put('jane', 20);
+        $this->map->put('john', 30);
+
+        self::assertTrue($this->map->containsKey('jane'));
+    }
+
+    public function testContainsValue(): void {
+        $this->map->put('jane', 20);
+        $this->map->put('john', 30);
+
+        self::assertTrue($this->map->containsKey('jane'));
+    }
+}
